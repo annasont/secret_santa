97f4bc13cc124a34dd465ddffaea845c083871d3
Anna Sontowska <aniasontowska@gmail.com>
2021-04-07 20:07:01 +0200

    Added context to home function, modified form

--- draw/views.py
+++ draw/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    context = {
+        'title': 'Home',
+        'no': [0, 1, 2]
+    }
     return render(request, 'draw/home.html', {'title': 'Home'})
