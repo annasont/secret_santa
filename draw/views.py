614b388450c5e91868ca651c47cd6a7efcb89c89
Anna Sontowska <aniasontowska@gmail.com>
2021-04-08 18:57:45 +0200

    Added GET-method to button

--- draw/views.py
+++ draw/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    context = {
-        'title': 'Home',
-        'no': [1, 2, 3]
-    }
+
+    if request.method == 'GET':
+        context = {
+            'title': 'Home',
+            'no': [1, 2, 3]
+        }
     return render(request, 'draw/home.html', context)
