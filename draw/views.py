efba938d929ba423c1440ad5954acd587d12884d
Anna Sontowska <aniasontowska@gmail.com>
2021-04-09 18:20:28 +0200

    Adding rows after clicking button

--- draw/views.py
+++ draw/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    if request.GET and request.GET['rows']:
+        no = int(request.GET['rows']) + 1
+    else:
+        no = 3
+  
     context = {
         'title': 'Home',
-        'no': 3
+        'no': no
     }
     return render(request, 'draw/home.html', context)
