1603a7daeb7eca85363ce2d8198eceddedc4783e
Anna Sontowska <aniasontowska@gmail.com>
2021-04-25 14:35:06 +0200

    Adding and removing rows in form works

--- draw/views.py
+++ draw/views.py
@@ -6,24 +6,32 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 
 def home(request):
-    extra = 3
+    ParticipantsFormset = formset_factory(ParticipantsForm, extra=3)
 
     valid = ''
     x = ''
 
     if request.method == 'POST':
         if request.POST['changeNoOfRows'] == 'add':
-            extra = int(float(request.POST['extra'])) + 1
-            # form = ParticipantsForm(initial=request.POST)
-            ParticipantsFormset = formset_factory(ParticipantsForm, extra=extra)
-            formset = ParticipantsFormset(request.POST)
+            cp = request.POST.copy()
+            cp['form-TOTAL_FORMS'] = int(cp['form-TOTAL_FORMS']) + 1
+            formset = ParticipantsFormset(cp)
+        elif request.POST['changeNoOfRows'] == 'subtract':
+            cp = request.POST.copy()
+            cp['form-TOTAL_FORMS'] = int(cp['form-TOTAL_FORMS']) - 1
+            formset = ParticipantsFormset(cp)
+
+
         if formset.is_valid():
             valid = request.POST
+
     else:
         noOfRows = 3
         ParticipantsFormset = formset_factory(ParticipantsForm, extra=noOfRows)
         formset = ParticipantsFormset() 
  
+    
+
     context = {
         'title': 'Home',
         'formset': formset,
