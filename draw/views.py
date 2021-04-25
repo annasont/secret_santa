068722dd0a2a02254c714e5490cb8a4e2c8e7cb1
Anna Sontowska <aniasontowska@gmail.com>
2021-04-25 12:24:57 +0200

    Added crispy-forms

--- draw/views.py
+++ draw/views.py
@@ -6,22 +6,29 @@ from django.utils.datastructures import MultiValueDictKeyError
 
 
 def home(request):
-    noOfRows = 3
-    ParticipantsFormset = formset_factory(ParticipantsForm, extra=noOfRows)
-    formset = ParticipantsFormset()
+    extra = 3
 
     valid = ''
+    x = ''
+
     if request.method == 'POST':
-        formset = ParticipantsFormset(request.POST)
+        if request.POST['changeNoOfRows'] == 'add':
+            extra = int(float(request.POST['extra'])) + 1
+            # form = ParticipantsForm(initial=request.POST)
+            ParticipantsFormset = formset_factory(ParticipantsForm, extra=extra)
+            formset = ParticipantsFormset(request.POST)
         if formset.is_valid():
             valid = request.POST
     else:
+        noOfRows = 3
+        ParticipantsFormset = formset_factory(ParticipantsForm, extra=noOfRows)
         formset = ParticipantsFormset() 
  
     context = {
         'title': 'Home',
         'formset': formset,
-        'valid': valid
+        'valid': valid,
+        'x': x
     }
     return render(request, 'draw/home.html', context)
 
