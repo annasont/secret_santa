42ec99d59ea331ce94a3b8ef3c393aa3f49dc094
Anna Sontowska <aniasontowska@gmail.com>
2021-04-26 20:20:58 +0200

    Bootstrap-look on form

--- draw/forms.py
+++ draw/forms.py
@@ -3,6 +3,12 @@ from .models import Participant
   
 
 class ParticipantsForm(forms.ModelForm):
+    name = forms.CharField(label='Name',
+                           max_length=70,
+                           widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email',
+                            max_length=100,
+                            widget=forms.EmailInput(attrs={'class': 'form-control'}))
     class Meta:
         model = Participant
         fields = "__all__"
