95dee1740cac6cf4fb082c3b41fc966026e44579
Anna Sontowska <aniasontowska@gmail.com>
2021-04-27 20:25:59 +0200

    Correted design of forms

--- draw/forms.py
+++ draw/forms.py
@@ -4,11 +4,11 @@ from .models import Participant
 
 class ParticipantsForm(forms.ModelForm):
     name = forms.CharField(label='Name',
-                           max_length=70,
-                           widget=forms.TextInput(attrs={'class': 'form-control'}))
+                            max_length=70,
+                            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Imię'}))
     email = forms.EmailField(label='Email',
                             max_length=100,
-                            widget=forms.EmailInput(attrs={'class': 'form-control'}))
+                            widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
     class Meta:
         model = Participant
         fields = "__all__"
