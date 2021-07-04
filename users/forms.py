9aef7195da665450468d1f9ea0c2576c97826178
Anna Sontowska <aniasontowska@gmail.com>
2021-07-04 17:45:31 +0200

    Added bootstrap formatting to register form

--- users/forms.py
+++ users/forms.py
@@ -5,31 +5,18 @@ from django.contrib.auth.forms import UserCreationForm
 # from django.utils.translation import ugettext_lazy as _
 
 class UserRegisterForm(UserCreationForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
     email = forms.EmailField()
-
-    password1 = forms.CharField(
-        label='Hasło',
-        strip=False,
-        widget=forms.PasswordInput(),
-        help_text='Twoje hasło musi zawierać co najmniej 8 znaków.\nTwoje hasło nie może być zbyt popularne.\nTwoje hasło nie może składać się wyłącznie z cyfr.'
-    )
-    password2 = forms.CharField(
-        label='Powtórz hasło',
-        widget=forms.PasswordInput(),
-        strip=False,
-        help_text='Wprowadź takie same hasło jak powyżej.',
-    )
-
-    error_messages = {
-        'password_mismatch': "Hasła muszą być takie same.",
-    }
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
-        labels = {
-            'username': 'Nazwa użytkownika',
-        }
         help_texts = {
-            'username': 'Maksymalnie 150 znaków. Używaj wyłącznie liter, cyfr oraz @/./+/-/_.'
+            'username': '150 characters or fewer. Letters, digits and @/./+/-/_ only.',
         }
+       
