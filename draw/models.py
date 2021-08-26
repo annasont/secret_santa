12db32fcf148f55d3c5cb3b558a01bfa721116cb
Anna Sontowska <aniasontowska@gmail.com>
2021-08-26 19:44:55 +0200

    database fixed

--- draw/models.py
+++ draw/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 class Participant(models.Model):
     name = models.CharField(max_length=70)
     email = models.EmailField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     def __str__(self):
         return f'{self.name}: {self.email}'
+
