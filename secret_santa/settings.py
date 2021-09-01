072f0032e949a6544a837ef86ad5c7c2bbd28e74
Anna Sontowska <aniasontowska@gmail.com>
2021-09-01 17:48:40 +0200

    added drawsecretsanta.herokuapp.com to allowed hosts.

--- secret_santa/settings.py
+++ secret_santa/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'drawsecretsanta.herokuapp.com']
 
 
 # Application definition
